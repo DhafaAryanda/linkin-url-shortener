8014e9b9d3222a8d9675c9b03a513a42b96912d9
dhofaaryanda4@gmail.com <dhofaaryanda4@gmail.com>
2025-06-09 23:56:42 +0700

    update error deployment

--- url-shortener-fe/src/app/page.tsx
+++ url-shortener-fe/src/app/page.tsx
@@ -53,9 +53,6 @@ export default function Home() {
 
       const response = await fetch(`${BACKEND_SERVER}/api/shorten`, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
         body: JSON.stringify({ url }),
       });
 
